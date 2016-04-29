9933d64345d8496245641999bd19ec787fad81fe
steve briceno sotomayor <steve briceno sotomayor>
2016-04-28 20:35:15 -0400

    Added new images and modified the food info activity layout

--- mobile/src/main/java/com/cs3714/vt_eats/MainActivity.java
+++ mobile/src/main/java/com/cs3714/vt_eats/MainActivity.java
@@ -28,13 +28,45 @@ public class MainActivity extends AppCompatActivity {
         ListAdapter DiningHallsAdapter = new DiningHallCustomAdapter(this, DiningHallList);
         lv = (ListView) findViewById(R.id.listView);
         lv.setAdapter(DiningHallsAdapter);
-
         lv.setOnItemClickListener(
                 new AdapterView.OnItemClickListener() {
                     @Override
                     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
+                        int DiningHallSelection = 0;
+
+                        switch (position) {
+                            case 0:  DiningHallSelection = 0;
+                                break;
+                            case 1:  DiningHallSelection = 1;
+                                break;
+                            case 2:  DiningHallSelection = 2;
+                                break;
+                            case 3:  DiningHallSelection = 3;
+                                break;
+                            case 4:  DiningHallSelection = 4;
+                                break;
+                            case 5:  DiningHallSelection = 5;
+                                break;
+                            case 6:  DiningHallSelection = 6;
+                                break;
+                            case 7:  DiningHallSelection = 7;
+                                break;
+                            case 8:  DiningHallSelection = 8;
+                                break;
+                            case 9:  DiningHallSelection = 9;
+                                break;
+                            case 10: DiningHallSelection = 10;
+                                break;
+                            case 11: DiningHallSelection = 11;
+                                break;
+                            case 12: DiningHallSelection = 12;
+                                break;
+                            default: break;
+                        }
+
                         Intent intent = new Intent(MainActivity.this, FoodInfoActivity.class);
+                        intent.putExtra("DINING_HALL_SELECTION", DiningHallSelection);
                         startActivity(intent);
                     }
                 }
