0a9f5013126fa02642aa43732100c756a1bce1c8
brobohn <brobohn@vt.edu>
2016-04-29 00:17:05 -0400

    Class stubs

---
+++ mobile/src/main/java/com/cs3714/vt_eats/DiningHall.java
@@ -0,0 +1,19 @@
+package com.cs3714.vt_eats;
+
+import android.location.Location;
+
+import java.util.HashMap;
+
+/**
+ * Created by Ben on 4/29/2016.
+ */
+public class DiningHall {
+    String name;
+    Location location;
+    HashMap<String, FoodItem> menu;
+
+    public DiningHall() {
+        
+    }
+
+}
---
+++ mobile/src/main/java/com/cs3714/vt_eats/DiningHallManager.java
@@ -0,0 +1,23 @@
+package com.cs3714.vt_eats;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Ben on 4/29/2016.
+ */
+public class DiningHallManager {
+
+    private ArrayList<DiningHall> diningHalls;
+
+    public DiningHallManager() {
+        diningHalls = new ArrayList<DiningHall>();
+
+        // build list here
+    }
+
+    public ArrayList<DiningHall> getDiningHalls() {
+        return diningHalls;
+    }
+
+
+}
