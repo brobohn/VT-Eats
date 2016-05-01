55b3207c0f06de10bf81a453e3a0316ce39ed5b1
steve briceno sotomayor <steve briceno sotomayor>
2016-05-01 13:58:54 -0400

    added images

--- mobile/src/main/java/com/cs3714/vt_eats/MainActivity.java
+++ mobile/src/main/java/com/cs3714/vt_eats/MainActivity.java
@@ -47,7 +47,6 @@ public class MainActivity extends AppCompatActivity {
                     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
 
-
                     }
                 }
         );
